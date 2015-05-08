ae3332423fdfc0f580f7a2f89b3744cbfb81ad31
Pierre Hilt <pierre.hilt@gmail.com>
2015-05-08 17:38:54 +0200

    Correct error of injection and reload

--- tasks/index-html.js
+++ tasks/index-html.js
@@ -3,25 +3,19 @@ var inject = require('gulp-inject');
 var watch = require('gulp-watch');
 var rename = require("gulp-rename");
 
-var injectApp = function() {
+var injects = function() {
   var appStream = gulp.src(includes.app, {read: false});
   var source = gulp.src('public/index.template.html');
-  return source.pipe(rename("index.html"))
+  var vendorsStream = gulp.src(includes.npm, { base: './node_modules' })
+    .pipe(gulp.dest('./public/lib'));
+  return source.pipe(inject(vendorsStream, {relative: true, name:"vendors"}))
     .pipe(inject(appStream, {relative: true}))
     .pipe(gulp.dest('views'));
 };
 
-var injectLib = function() {
-  var appStream = gulp.src(includes.app, {read: false});
-  var vendorsStream = gulp.src(includes.npm, { base: './node_modules' })
-    .pipe(gulp.dest('./public/lib'));
-  return injectApp().pipe(inject(vendorsStream, {relative: true, name:"vendors"}))
-    .pipe(inject(appStream, {relative: true}));
-};
-
 // Copy node_modules files to public/lib and inject them
 gulp.task("inject", function() {
-  return injectLib();
+  return injects();
 });
 
 // Watch modification on static files, dev environement, wait build to finish
@@ -29,12 +23,12 @@ gulp.task("watch:app", ["build:index"], function() {
   watch(includes.app, {verbose:true}, function(stream) {
     console.log(stream.event);
     if (stream.event == "add" || stream.event == "unlink") {
-      injectLib();
+      injects();
     }
   });
   watch('public/index.template.html', {verbose:true}, function(stream) {
     console.log(stream.event);
-    injectLib();
+    injects();
   });
 
 });
