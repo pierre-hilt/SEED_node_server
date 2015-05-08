6f515bf7bcd1b6fdf7d5f0159fc307c4f6043e0c
Pierre Hilt <pierre.hilt@gmail.com>
2015-05-08 16:31:31 +0200

    Correct error when adding js files

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var swig = require('swig');
+var consolidate = require('consolidate');
 var config = require("./config/config");
 
 var ENV = process.env.ENV || "PROD";
 
 app.set('port', (process.env.PORT || 3000));
 
-app.engine('html', swig.renderFile);
+
+// Set swig as the template engine
+app.engine('html', consolidate.swig);
+
+// Set views path and view engine
 app.set('view engine', 'html');
-app.set('views', __dirname + '/public');
+app.set('views', './views');
+
+app.use(express.static(path.resolve("./public")));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
--- tasks/index-html.js
+++ tasks/index-html.js
@@ -1,30 +1,27 @@
 var includes = require("../config/includes.js");
 var inject = require('gulp-inject');
 var watch = require('gulp-watch');
+var rename = require("gulp-rename");
 
 var injectApp = function() {
   var appStream = gulp.src(includes.app, {read: false});
-  var target = gulp.src('public/index.html');
-  return target.pipe(inject(appStream, {relative: true}))
-    .pipe(gulp.dest('public'));
+  var source = gulp.src('public/index.template.html');
+  return source.pipe(inject(appStream, {relative: true}))
+    .pipe(rename("index.html"))
+    .pipe(gulp.dest('views'));
 };
 
 var injectLib = function() {
-  var target = gulp.src('./public/index.html');
+  var appStream = gulp.src(includes.app, {read: false});
   var vendorsStream = gulp.src(includes.npm, { base: './node_modules' })
     .pipe(gulp.dest('./public/lib'));
-  return target.pipe(inject(vendorsStream, {relative: true, name:"vendors"}))
-    .pipe(gulp.dest('./public'));
+  return injectApp().pipe(inject(vendorsStream, {relative: true, name:"vendors"}))
+    .pipe(inject(appStream, {relative: true}));
 };
 
 // Copy node_modules files to public/lib and inject them
-gulp.task("inject:lib", ["inject:app"], function() {
+gulp.task("inject", function() {
   return injectLib();
-});
-
-// Inject static files
-gulp.task("inject:app", function() {
-  return injectApp();
 });
 
 // Watch modification on static files, dev environement, wait build to finish
@@ -38,4 +35,4 @@ gulp.task("watch:app", ["build:index"], function() {
 });
 
 // build index.html
-gulp.task("build:index", ["inject:app", "inject:lib"]);
+gulp.task("build:index", ["inject"]);
